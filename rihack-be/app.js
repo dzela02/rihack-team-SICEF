bf6969feb196df0bb1f3bc538d27ff3e931ab170
AndjelijaMladenovic00 <andjelija.mladenovic@elfak.rs>
2022-07-23 16:10:51 +0200

    Usser session and controllers

--- rihack-be/app.js
+++ rihack-be/app.js
@@ -13,7 +13,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
 app.use(logger("dev"));
-app.use(express.json());
+app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
@@ -32,8 +32,9 @@ app.use(function (err, req, res, next) {
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+  res.status(err.status || 500).json({
+    error: err.message,
+  });
 });
 
 module.exports = app;
